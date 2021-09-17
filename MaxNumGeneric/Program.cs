48d1c9de256df50544bcbbf51e4c6d3f9090a927
sonupooja <ingalahallipooja@gmail.com>
2021-09-17 22:54:36 +0530

    Adding maxnum Program

--- MaxNumGeneric/Program.cs
+++ MaxNumGeneric/Program.cs
@@ -4,9 +4,12 @@ namespace MaxNumGeneric
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//main method
         {
             Console.WriteLine("Welcome to Generic Program");
+            Console.WriteLine("Maximum Number is :");
+            int output = MaximumNumber.MaximumInteger(90, 80, 30);//calling method
+            Console.WriteLine(output);
         }
     }
 }

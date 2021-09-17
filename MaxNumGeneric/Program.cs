a3506abdb281f5e788dd169d8e6eb33548554873
sonupooja <ingalahallipooja@gmail.com>
2021-09-17 22:33:53 +0530

    Welcome to Generic Program

---
+++ MaxNumGeneric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxNumGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Program");
+        }
+    }
+}

da0cddb7e42bef11abace38a0c86db5ce4277f02
sonupooja <ingalahallipooja@gmail.com>
2021-09-17 23:17:46 +0530

    Adding maxnum of string Program

--- MaxNumGeneric/Program.cs
+++ MaxNumGeneric/Program.cs
@@ -9,7 +9,8 @@ namespace MaxNumGeneric
             Console.WriteLine("Welcome to Generic Program");
             Console.WriteLine("Maximum Number is :");
             int output = MaximumNumber.MaximumInteger(90, 80, 30);// calling method for integer
-            double doubleoutput = MaximumNumber.MaximumFloat(30.23, 42.43, 53.22); //calling method for float
+            double doubleoutput = MaximumNumber.MaximumFloat(30.23, 42.43, 53.22);//calling method for float
+            string stringoutput = MaximumNumber.MaximumString("40", "60", "85");  //calling method for string
             Console.WriteLine(output);
         }
     }
